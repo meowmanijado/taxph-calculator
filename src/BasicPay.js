c39c12b384de2a73d86fb0db568df44becf4f87c
Sharmaine Manijado (DELE) <sharmainemanijado@gmail.com>
2017-10-03 20:35:50 +0800

    Added some methods.

--- src/BasicPay.js
+++ src/BasicPay.js
@@ -1,21 +1,27 @@
 import React from 'react';
 
+
 export class BasicPay extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			period: 'semi-monthly',
+			period: 'monthly',
 			basicSalary: 0,
 			status: 'single',
 			sss: 0,
 			philhealth: 0,
-			pagibig: 100
+			pagibig: 0,
+			totalDeduction: 0,
+			taxableIncome: 0
 		};
 
 		this.handleUserInput = this.handleUserInput.bind(this);
 		this.handleChange = this.handleChange.bind(this);
-		this.philhealthContrib = this.philhealthContrib.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.getPhilhealth = this.getPhilhealth.bind(this);
+		this.sssContribution = this.sssContribution.bind(this);
+		this.getTaxableIncome = this.getTaxableIncome.bind(this);
 	}
 
 	handleUserInput(e) {
@@ -29,72 +35,114 @@ export class BasicPay extends React.Component {
 	handleSubmit(e) {
 		e.preventDefault();
 
-		alert('foo');
+		this.getTaxableIncome();
 	}
 
-	philhealthContrib(e) {
-		e.preventDefault();
-		const newPhilhealth = this.state.basicSalary === 0 ? 100 : 200;
+	getPhilhealth() {
+		const philhealthContrib = this.state.basicSalary;
 
-		this.setState({philhealth: newPhilhealth});
+		this.setState({philhealth: philhealthContrib});
+
+		return philhealthContrib;
 	}
 
-	// sssContribution(e) {
-	// 	e.preventDefault();
+	sssContribution() {
 
-	// 	const employeeContrib = 0.0363;
-	// 	const totalContrib = employeeContrib * this.state.basicSalary;
-	// 	const sssContrib = Math.ceil(totalContrib);
+		const employeeContrib = 0.0363;
+		const totalContrib = employeeContrib * this.state.basicSalary;
 
-	// 	this.setState({sss: sssContrib})
-	// }
+		if (this.state.period === 'semi-monthly') {
+
+			this.setState({sss: Math.ceil(totalContrib) / 2});
+
+			return  Math.ceil(totalContrib) / 2;
+
+		}
+
+		this.setState({sss: Math.ceil(totalContrib)});
+
+		return Math.ceil(totalContrib);
+		
+	}
+
+	getPagibig() {
+
+		const pagibigContrib = this.state.period === 'semi-monthly' ?  50 : 100;
+
+		this.setState({pagibig: pagibigContrib});
+
+		return pagibigContrib;
+	}
+
+	getTotalDeduction() {
+		const totalDeduction = this.sssContribution() + this.getPhilhealth() + this.getPagibig();
+
+		this.setState({totalDeduction: totalDeduction});
+
+		return totalDeduction;
+	}
+
+	getTaxableIncome() {
+		// Basic salary - (sss + philhealth + pagibig)
+		const basicSalary = this.state.basicSalary;
+		
+		const deduction = this.getTotalDeduction();
+
+		this.setState({taxableIncome: basicSalary - deduction});
+	}
 
 	render() {
-		// const getPeriod = this.state.period === 'semi-monthly' ? 10 : 20;
-		// const getStatus = this.state.status === 'single' ? 100 : 200;
 
 		return (
-			<form onSubmit={this.handleSubmit}>
-				<select value={this.state.period} onChange={this.handleChange} name="period">
-					<option value="semi-monthly">Semi-Monthly</option>
-					<option value="monthly">Monthly</option>
-				</select>
-				<input 
-					type="text" 
-					name="basicSalary"
-					onChange={this.handleUserInput}
-					value={this.state.basicSalary} />
-				<select value={this.state.status} onChange={this.handleChange} name="status">
-					<option value="single">Single</option>
-					<option value="married">Married</option>
-				</select>
-				<p>sss: 
-				<input 
-					type="text" 
-					name="sss"
-					onChange={this.handleUserInput}
-					value={this.state.sss} />
+			<form onSubmit={this.handleSubmit}>	
+				<p>Basic Pay</p>
+				<p>Period:
+					<select value={this.state.period} onChange={this.handleChange} name="period">
+						<option value="monthly">Monthly</option>
+						<option value="semi-monthly">Semi-Monthly</option>
+					</select>
 				</p>
-				<p>philhealth: 
-				<input 
-					type="text" 
-					name="philhealth"
-					onChange={this.philhealthContrib}
-					value={this.state.philhealth} />
+				<p>Basic Salary:
+					<input 
+						type="text" 
+						name="basicSalary"
+						onChange={this.handleUserInput}
+						value={this.state.basicSalary} />
 				</p>
-				<p>pagibig: 
-				<input 
-					type="text" 
-					name="pagibig"
-					onChange={this.handleUserInput}
-					value={this.state.pagibig} />
+				<p>Status:
+					<select value={this.state.status} onChange={this.handleChange} name="status">
+						<option value="single">Single</option>
+						<option value="married">Married</option>
+					</select>
 				</p>
-				 <input type="submit" value="Submit" />
+				<p>Deductions</p>
+				<p>SSS: 
+					<input 
+						type="text" 
+						name="sss"
+						value={this.state.sss} />
+				</p>
+				<p>Philhealth: 
+					<input 
+						type="text" 
+						name="philhealth"
+						value={this.state.philhealth} />
+				</p>
+				<p>Pagibig: 
+					<input 
+						type="text" 
+						name="pagibig"
+						value={this.state.pagibig} />
+				</p>
+				<p>Total Deductions: {this.state.totalDeduction}</p>
+				<p>Taxable Income: {this.state.taxableIncome}</p>
+				<p>Withholding Tax: </p>
+				<p>Net Income: </p>
+
+				<input type="submit" value="Submit" />
 			</form>
 		);
 	}
 }
-
-
 
 export default BasicPay;
