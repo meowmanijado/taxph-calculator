0006cb2e959e5d8af23b81b4b376bb86ab49a343
Sharmaine Manijado (DELE) <sharmainemanijado@gmail.com>
2017-12-01 19:34:00 +0800

    Update css total

--- src/Total.js
+++ src/Total.js
@@ -5,11 +5,31 @@ export class Total extends React.Component {
 	render() {
 		return (
 			<div>
-				<p>Total Basic Pay: {this.props.totalBasicPay}</p>
-				<p>Total Deductions: {this.props.totalDeduction}</p>
-				<p>Taxable Income: {this.props.taxableIncome}</p>
-				<p>Withholding Tax: {this.props.withholdingTax}</p>
-				<p>Net Income: {this.props.netIncome}</p>
+				<div className="mb-4">
+					<label className="block text-grey-darker text-sm font-bold mb-2" for="username">
+				        Total Basic Pay: {this.props.totalBasicPay}
+				    </label>
+				</div>
+				<div className="mb-4">
+					<label className="block text-grey-darker text-sm font-bold mb-2" for="username">
+				        Total Deductions: {this.props.totalDeduction}
+				    </label>
+				</div>
+				<div className="mb-4">
+					<label className="block text-grey-darker text-sm font-bold mb-2" for="username">
+				        Taxable Income: {this.props.taxableIncome}
+				    </label>
+				</div>
+				<div className="mb-4">
+					<label className="block text-grey-darker text-sm font-bold mb-2" for="username">
+				        Withholding Tax: {this.props.withholdingTax}
+				    </label>
+				</div>
+				<div className="mb-4">
+					<label className="block text-grey-darker text-sm font-bold mb-2" for="username">
+				        Net Income: {this.props.netIncome}
+				    </label>
+				</div>
 			</div>
 		)
 	}
