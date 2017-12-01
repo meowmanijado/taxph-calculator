38991c9b2dd63e296a1a52794aa2a1d7827b7e0d
Sharmaine Manijado (DELE) <sharmainemanijado@gmail.com>
2017-12-01 19:31:04 +0800

    Update css deduction

--- src/Deductions.js
+++ src/Deductions.js
@@ -6,28 +6,41 @@ export class Deductions extends React.Component {
 	render() {
 		return (
 			<div>
-				<h4>Deductions</h4>
-				<p>SSS: 
-					<input 
-						type="text" 
-						name="sss"
-						onChange={this.props.onChange}
-						value={this.props.sss} />
-				</p>
-				<p>Philhealth: 
-					<input 
-						type="text" 
-						name="philhealth"
-						onChange={this.props.onChange}
-						value={this.props.philhealth} />
-				</p>
-				<p>Pagibig: 
-					<input 
-						type="text" 
-						name="pagibig"
-						onChange={this.props.onChange}
-						value={this.props.pagibig} />
-				</p>
+				<div className="mb-4">
+					<label className="block text-grey-darker text-sm font-bold mb-2" for="username">
+				        Deductions
+				     </label>
+				</div>
+				<div className="mb-4">
+					<label className="block text-grey-darker text-sm font-bold mb-2" for="username">
+				        SSS:
+				    </label>
+				    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+				    		type="text"
+				    		name="sss"
+				    		onChange={this.props.onChange}
+				    		value={this.props.sss} />
+				</div>
+				<div className="mb-4">
+					<label className="block text-grey-darker text-sm font-bold mb-2" for="username">
+				        Philhealth:
+				    </label>
+				    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+				    		type="text"
+				    		name="philhealth"
+				    		onChange={this.props.onChange}
+				    		value={this.props.philhealth} />
+				</div>
+				<div className="mb-4">
+					<label className="block text-grey-darker text-sm font-bold mb-2" for="username">
+				        Pagibig:
+				    </label>
+				    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+				    		type="text"
+				    		name="pagibig"
+				    		onChange={this.props.onChange}
+				    		value={this.props.pagibig} />
+				</div>
 			</div>
 		)
 	}
